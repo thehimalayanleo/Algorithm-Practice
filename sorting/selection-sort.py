37cb1338a4091c46a4ef7c2332127479641c346c
ajinkyamulay123@gmail.com <ajinkyamulay123@gmail.com>
2019-08-31 14:20:49 -0400

    Updated partial selection sort

---
+++ sorting/selection-sort.py
@@ -0,0 +1,8 @@
+## Implementation of a selection sort for integers (or any other element, which sorts by using simple greater than or smaller than comparators). By
+## adding mutlipele defintions of the comparator, it shoudl be possible to make this program generic.
+
+class sorting:
+    def __init__(self):
+        self.arr = [] # creates a blank array
+
+    def selection_sort(self):
